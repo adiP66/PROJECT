0b8b07c88a4118aafa0604bece44ee3d8e215851
adiP66 <95730600+adiP66@users.noreply.github.com>
2023-09-08 19:31:33 +0530

    Delete Home page directory

--- Home page/script.js
+++
@@ -1,8 +0,0 @@
-const togglebtn = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-
-togglebtn.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
